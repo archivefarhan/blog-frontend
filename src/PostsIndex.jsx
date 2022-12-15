193cafd673126a8f62518522b66ba9d96831f7e3
farhan <farhanj.mohamed@gmail.com>
2022-12-14 17:53:24 -0800

    removed a duplicate class name

--- src/PostsIndex.jsx
+++ src/PostsIndex.jsx
@@ -5,7 +5,7 @@ export function PostsIndex(props) {
     <div id="posts-index">
       <h1 className="text-center">All posts</h1>
       {props.posts.map((post) => (
-        <div key={post.id} className="posts" className="mt-1 container text-center">
+        <div key={post.id} className="posts mt-1 container text-center">
           <h2 className="mt-3">{post.title}</h2>
           <img src={post.image} alt="" />
           <p className="mt-3">Body: {post.body}</p>
